00193bbb39e3c3d30332cafb6c8953edfccc7b05
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-05-20 23:19:49 +0000

    app ransactions (admin.py)

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,6 +4,6 @@ from .views import InitiateTransactionView, TransactionHistoryView, DiscountCode
 urlpatterns = [
     path('initiate/', InitiateTransactionView.as_view(), name='initiate_transaction'),
     path('history/', TransactionHistoryView.as_view(), name='transaction_history'),
-    path('discount-codes/create/', DiscountCodeCreateView.as_view(), name='create_discount_code'), # Nouvelle URL pour créer
-    path('discount-codes/', DiscountCodeListView.as_view(), name='list_discount_codes'), # Nouvelle URL pour lister
+    # path('discount-codes/create/', DiscountCodeCreateView.as_view(), name='create_discount_code'), # Nouvelle URL pour créer
+    # path('discount-codes/', DiscountCodeListView.as_view(), name='list_discount_codes'), # Nouvelle URL pour lister
 ]
