637fb43772c88680e067d845ce6fa499004ba4be
OTISDav <159715854+OTISDav@users.noreply.github.com>
2025-05-20 23:03:59 +0000

    app ransactions

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Wallet
 
-# Register your models here.
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'balance', 'created_at', 'updated_at')  # Ajoute ici les champs existants
+    search_fields = ('user__username', 'user__phone')
+    list_filter = ('created_at',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user')
